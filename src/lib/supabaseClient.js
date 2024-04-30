8dae040fa08407aadf1188fccb39fcfeab3d24ea
timmcgeegit <145072972+timmcgeegit@users.noreply.github.com>
2024-04-30 13:16:49 -0500

    again again again

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,7 +1,5 @@
-// ccchouston/src/lib/supabaseClient.js
+// src/lib/supabaseClient.js
 import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = process.env.PUBLIC_SUPABASE_URL;
-const supabaseKey = process.env.PUBLIC_SUPABASE_ANON_KEY;
-
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_ANON_KEY;
 export const supabase = createClient(supabaseUrl, supabaseKey);
